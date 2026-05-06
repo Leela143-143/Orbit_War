997f851b9a0231e3944a1cc868254025b43b1bf4
Leela Guna Vardhan Killada <146460946+Leela143-143@users.noreply.github.com>
2026-05-06 09:36:04 +0530

    Change player scripts from new.py to improvement.py

--- play_single_game.py
+++ play_single_game.py
@@ -7,7 +7,7 @@ def play_one_game():
     env = make("orbit_wars", debug=True)
     
     print("Running a single game...")
-    env.run(["new.py", "main.py"])
+    env.run(["improvement.py", "main.py"])
     
     print("\nGame Over!")
     
